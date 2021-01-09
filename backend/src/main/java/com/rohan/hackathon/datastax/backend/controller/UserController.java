8c0ed1156d62907c0dfdfb9589d3e3cafe400f4c
Rohan Prasad <rohan.prasad@tsgforce.com>
2021-01-09 21:33:32 +0530

    Clean up code.

--- backend/src/main/java/com/rohan/hackathon/datastax/backend/controller/UserController.java
+++ backend/src/main/java/com/rohan/hackathon/datastax/backend/controller/UserController.java
@@ -1,14 +1,16 @@
 package com.rohan.hackathon.datastax.backend.controller;
 
+import com.rohan.hackathon.datastax.backend.exception.AuthenticationException;
+import com.rohan.hackathon.datastax.backend.exception.LoginException;
+import com.rohan.hackathon.datastax.backend.model.JwtRequest;
+import com.rohan.hackathon.datastax.backend.model.JwtResponse;
 import com.rohan.hackathon.datastax.backend.model.User;
 import com.rohan.hackathon.datastax.backend.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
@@ -30,6 +32,22 @@ public class UserController {
         } else {
             return ResponseEntity.badRequest().build();
         }
+    }
 
+    @PostMapping(value = "/user/login")
+    public ResponseEntity<JwtResponse> createAuthenticationToken(@RequestBody JwtRequest authenticationRequest) throws Exception {
+        logger.info("Received request to login: {}", authenticationRequest);
+        String token = this.userService.login(authenticationRequest);
+        return ResponseEntity.ok(new JwtResponse(token));
+    }
+
+    @ExceptionHandler({AuthenticationException.class})
+    public ResponseEntity<String> handleAuthenticationException(AuthenticationException e) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+    }
+
+    @ExceptionHandler({LoginException.class})
+    public ResponseEntity<String> handleLoginException(LoginException e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
     }
 }
--- backend/src/main/java/com/rohan/hackathon/datastax/backend/service/UserService.java
+++ backend/src/main/java/com/rohan/hackathon/datastax/backend/service/UserService.java
@@ -1,9 +1,19 @@
 package com.rohan.hackathon.datastax.backend.service;
 
+import com.rohan.hackathon.datastax.backend.exception.AuthenticationException;
+import com.rohan.hackathon.datastax.backend.exception.LoginException;
+import com.rohan.hackathon.datastax.backend.model.JwtRequest;
 import com.rohan.hackathon.datastax.backend.model.User;
 import com.rohan.hackathon.datastax.backend.repository.UserRepository;
+import com.rohan.hackathon.datastax.backend.util.JwtTokenUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -12,12 +22,40 @@ public class UserService {
 
     private static final Logger logger = LoggerFactory.getLogger(UserService.class);
 
+    private final JwtTokenUtil jwtTokenUtil;
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final UserDetailsService jwtUserDetailsService;
+    private final AuthenticationManager authenticationManager;
 
-    public UserService(final UserRepository userRepository, final PasswordEncoder passwordEncoder) {
+
+    public UserService(final JwtTokenUtil jwtTokenUtil, final UserRepository userRepository, final PasswordEncoder passwordEncoder, final JwtUserDetailsService jwtUserDetailsService, final AuthenticationManager authenticationManager) {
+        this.jwtTokenUtil = jwtTokenUtil;
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.jwtUserDetailsService = jwtUserDetailsService;
+        this.authenticationManager = authenticationManager;
+    }
+
+    public String login(JwtRequest authenticationRequest) {
+        authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
+
+        final UserDetails userDetails = jwtUserDetailsService
+                .loadUserByUsername(authenticationRequest.getUsername());
+        logger.info("Found user = {}.", userDetails);
+        return jwtTokenUtil.generateToken(userDetails);
+    }
+
+    private void authenticate(String username, String password) {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+        } catch (DisabledException | BadCredentialsException e) {
+            logger.error("Invalid credentials received: {}", e.getMessage());
+            throw new AuthenticationException(e.getMessage(), e);
+        } catch (Exception e) {
+            logger.error("Something went wrong while trying to login: {}", e.getMessage(), e);
+            throw new LoginException(e.getMessage(), e);
+        }
     }
 
     public boolean createNewUser(User user) {
