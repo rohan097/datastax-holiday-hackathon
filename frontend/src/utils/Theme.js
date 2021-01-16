5aed5f463bd778b67159127e0c9ef319aad4fc6e
Rohan Prasad <rohan.prasad@tsgforce.com>
2021-01-17 00:13:58 +0530

    UI improvements.

---
+++ frontend/src/utils/Theme.js
@@ -0,0 +1,7 @@
+export const DARK_PRIMARY_COLOR = "#0288D1";
+export const LIGHT_PRIMARY_COLOR = "#B3E5FC";
+export const PRIMARY_COLOR = "#03A9F4";
+export const TEXT_ICONS = "#FFFFFF";
+export const ACCENT_COLOR = "#8BC34A";
+export const PRIMARY_TEXT = "#212121";
+export const SECONDARY_TEXT = "#757575";
